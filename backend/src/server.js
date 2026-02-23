15a36439370b729d2ccf49077a0a5724c449e2f6
markramlich <krme23gb@student.ju.se>
2026-02-23 19:33:00 +0100

    feat(backend): add docker, postgres, set up initial server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,68 @@
-const { createServer } = require("node:http");
+const { Client } = require("pg");
+const express = require("express");
+const app = express();
 
-const hostname = "127.0.0.1";
-const port = 3000;
-
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+const client = new Client({
+  host: "localhost",
+  port: 5432,
+  user: "postgres",
+  password: "12345",
+  database: "postgres",
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get("/all", async (req, res) => {
+  try {
+    const dbres = await client.query("SELECT * FROM users;");
+    console.log("All users:", dbres.rows);
+    res.json(dbres.rows);
+  } catch (err) {
+    console.error("Error selecting records", err.stack);
+  }
 });
+
+function createTable() {
+  const createTableQuery = `
+CREATE TABLE IF NOT EXISTS users (
+id SERIAL PRIMARY KEY,
+username VARCHAR(50),
+email VARCHAR(100) UNIQUE,
+password VARCHAR(255)
+); 
+`;
+  client
+    .query(createTableQuery)
+    .then(() => console.log('Table "users" created or already exists'))
+    .catch((err) => console.error("Error creating table", err.stack));
+}
+
+function insertRecord(insertValues) {
+  const insertQuery = `
+INSERT INTO users (username, email, password)
+VALUES ($1, $2, $3)
+RETURNING *;
+`;
+  client
+    .query(insertQuery, insertValues)
+    .then((res) => console.log("Inserted record:", res.rows[0]))
+    .catch((err) => console.error("Error inserting record", err.stack));
+}
+
+// Change if exists
+// const insertValues = ["admin", "admin@gmail.com", "admin"];
+
+const startServer = async () => {
+  try {
+    await client.connect();
+    console.log("Connected to PostgreSQL database ");
+  } catch (err) {
+    console.error("Connection error", err.stack);
+  }
+  app.listen(3001, () => {
+    console.log("Example app listening on port 3000!");
+  });
+};
+
+startServer();
+
+createTable();
+// insertRecord(insertValues);
