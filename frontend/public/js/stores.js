6df7c095001aac6ee64ac1ca068036f8a59339cd
veronicasorge <150162661+veronicasorge@users.noreply.github.com>
2026-03-07 13:05:29 +0100

    filtering and sorting

--- frontend/public/js/stores.js
+++ frontend/public/js/stores.js
@@ -1,17 +1,48 @@
 const grid = document.querySelector(".grid");
+const districtFilter = document.getElementById("district-filter");
+const sortingSelect = document.getElementById("sorting");
+
+let allStores = [];
 
 async function loadStores() {
   try {
     const response = await fetch("http://localhost:3001/stores");
     if (!response.ok) throw new Error("Failed to fetch stores");
 
-    const stores = await response.json();
-    displayStores(stores);
+    allStores = await response.json();
+    applyFiltersAndSort();
   } catch (error) {
     console.error("Error:", error);
     grid.innerHTML = "<p>Could not load stores</p>";
   }
 }
+
+function applyFiltersAndSort() {
+  //create copy of the original stores to modify
+  let filtered = [...allStores];
+
+  //filter by district
+  const selectedDistrict = districtFilter.value;
+  if (selectedDistrict) {
+    filtered = filtered.filter(store => store.district === selectedDistrict);
+  }
+
+  //sort logic
+  const sortBy = sortingSelect.value;
+  filtered.sort((a,b) => {
+    if (sortBy === "az") {
+      return a.name.localeCompare(b.name);
+    }
+    if (sortBy === "district") {
+      //if store has no district, move it
+      return (a.district || "").localeCompare(b.district || "");
+    }
+    return 0;
+  });
+
+  displayStores(filtered);
+}
+
 
 function displayStores(stores) {
   grid.innerHTML = "";
@@ -40,6 +71,7 @@ function displayStores(stores) {
   });
 }
 
-document.addEventListener("DOMContentLoaded", async () => {
-  await loadStores();
-});
+districtFilter.addEventListener("change", applyFiltersAndSort);
+sortingSelect.addEventListener("change", applyFiltersAndSort);
+
+document.addEventListener("DOMContentLoaded", loadStores);
