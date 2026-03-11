6c6b5c91f3ad0429a39c7054710b5ac20934b7d6
ReinisMuiznieks <muizreinis@gmail.com>
2026-03-11 19:00:52 +0100

    feat(frontend): remove login form check

--- frontend/public/js/login.js
+++ frontend/public/js/login.js
@@ -1,35 +1,32 @@
 const loginForm = document.getElementById("login-form");
-if (loginForm) {
-  loginForm.addEventListener("submit", async (e) => {
-    e.preventDefault(); // stop the form from reloading the page
 
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault(); // stop the form from reloading the page
 
-    try {
-      // send data to backend
-      const response = await fetch("http://localhost:3000/users/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-        body: JSON.stringify({ email, password }),
-      });
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-      const data = await response.json();
+  try {
+    // send data to backend
+    const response = await fetch("http://localhost:3000/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({ email, password }),
+    });
 
-      if (response.ok) {
-        alert("Login successful!");
-        window.location.href = "/";
-      } else {
-        alert(data.error || "Login failed");
-      }
-    } catch (err) {
-      console.error("Connection Error:", err);
-      alert(
-        "Could not connect to the backend server. Make sure it is running!"
-      );
+    const data = await response.json();
+
+    if (response.ok) {
+      alert("Login successful!");
+      window.location.href = "/";
+    } else {
+      alert(data.error || "Login failed");
     }
-  });
-}
+  } catch (err) {
+    console.error("Connection Error:", err);
+    alert("Could not connect to the backend server. Make sure it is running!");
+  }
+});
